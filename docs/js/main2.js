c5f64f19c6e5705c0ba0c1f7e6015ebc1fad1d97
soarflat <soarflat777@gmail.com>
2017-09-21 18:16:35 +0900

    [update] promise test

--- docs/js/main2.js
+++ docs/js/main2.js
@@ -65,6 +65,33 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(global) {var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -7993,34 +8020,7 @@ if (_gsScope._gsDefine) { _gsScope._gsQueue.pop()(); } //necessary in case Tween
 		_tickerActive = false; //ensures that the first official animation forces a ticker.tick() to update the time when it is instantiated
 
 })((typeof(module) !== "undefined" && module.exports && typeof(global) !== "undefined") ? global : this || window, "TweenMax");
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-var g;
-
-// This works in non-strict mode
-g = (function() {
-	return this;
-})();
-
-try {
-	// This works if eval is allowed (see CSP)
-	g = g || Function("return this")() || (1,eval)("this");
-} catch(e) {
-	// This works if the window reference is available
-	if(typeof window === "object")
-		g = window;
-}
-
-// g can still be undefined, but nothing to do about it...
-// We return undefined, instead of nothing here, so it's
-// easier to handle this case. if(!global) { ...}
-
-module.exports = g;
-
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
 /* 2 */
@@ -12807,7 +12807,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/*! VelocityJS
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_gsap__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_gsap__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_gsap___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_gsap__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_velocity_animate__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_velocity_animate___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_velocity_animate__);
--- src/main3.js
+++ src/main3.js
@@ -1,29 +1,13 @@
-import { TweenMax } from 'gsap';
-import velocity from 'velocity-animate';
+import Promise from 'es6-promise';
 
-var topClipRightP = document.getElementById('topClipRightP');
-var topClipLeftP = document.getElementById('topClipLeftP');
+function promise() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000)
+  });
+}
 
-velocity(topClipLeftP, {
-  translateX: [0, '100%'],
-  translateY: [0, '100%']
-}, {
-  delay: 1000,
-  duration: 300
+promise().then(() => {
+  alert('resolve');
 });
-
-TweenMax.fromTo(topClipRightP, 0.5, {
-  attr: {
-    transform: 'translate(100,100)'
-  }
-}, {
-  attr: {
-    transform: 'translate(0,0)'
-  }
-});
-
-// TweenMax.from(topClipLeftP, 0.3, {
-//   attr: {
-//     points: '-200,335 175,620 -200,910'
-//   }
-// });
